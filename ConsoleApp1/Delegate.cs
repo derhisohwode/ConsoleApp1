2e123052b3cb8c41808e2a43fd22062d81cfb787
Daniel Erhisohwode <derhisohwode@gtlregistrars.com>
2023-02-14 17:01:22 +0100

    upda

--- ConsoleApp1/Delegate.cs
+++ ConsoleApp1/Delegate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Cragy
 {
@@ -13,11 +14,39 @@ namespace Cragy
         {
             delly del = new delly(Hello);
             del("resume ", 34);
-        }
 
-        public static void Hello(string Message, int dev)
-        {
-            Console.WriteLine(Message + dev);
-        }
-    }
+		List<Delegate> bltList = new List<Delegate>();
+
+			bltList.Add(new Delegate() { ID = 101, Name = "Mary", Salary = 5000, experience = 5 });
+			bltList.Add(new Delegate() { ID = 102, Name = "Mark", Salary = 3000, experience = 5 });
+			bltList.Add(new Delegate() { ID = 103, Name = "Mare", Salary = 4000, experience = 4 });
+			bltList.Add(new Delegate() { ID = 104, Name = "Marl", Salary = 6000, experience = 4 });
+
+			Delegate.PromotedDelegate(bltList);
+		}
+	public static void Hello(string Message, int dev)
+	{
+		Console.WriteLine(Message + dev);
+	}
+}
+
+
+class Delegate
+{
+	public int ID { get; set; }
+	public string Name { get; set; }
+	public int Salary { get; set; }
+	public int experience { get; set; }
+
+	public static void PromotedDelegate(List<Delegate> delegateList)
+	{
+		foreach (Delegate belegate in delegateList)
+		{
+			if (belegate.experience >= 5)
+			{
+				Console.WriteLine("promoted");
+			}
+		}
+	}
+}
 }
